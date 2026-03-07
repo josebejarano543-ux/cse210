5e85bfa8df8f4799a9b0bc657d43e67b3cd4d6ea
josebejarano543-ux <jose.bejarano543@gmail.com>
2026-03-07 16:25:33 -0700

    Completed Exercise 3 loops

---
+++ week01/Exercise1/Exercise2/Exercise3/Program.cs
@@ -0,0 +1,32 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            string userInput = Console.ReadLine();
+            guess = int.Parse(userInput);
+
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
+    }
+}

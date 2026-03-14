0bfd8fb61a676e3e77a101e6c4c3f7fa35e9818a
josebejarano543-ux <jose.bejarano543@gmail.com>
2026-03-14 17:17:04 -0600

    "Completed Journal Program"

---
+++ week02/Journal/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Journal journal = new Journal();
+        PromptGenerator promptGenerator = new PromptGenerator();
+
+        int choice = 0;
+
+        while (choice != 5)
+        {
+            Console.WriteLine("Welcome to the Journal Program!");
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Save");
+            Console.WriteLine("4. Load");
+            Console.WriteLine("5. Quit");
+            Console.Write("What would you like to do? ");
+
+            string input = Console.ReadLine();
+            choice = int.Parse(input);
+
+            if (choice == 1)
+            {
+                string prompt = promptGenerator.GetRandomPrompt();
+
+                Console.WriteLine($"Prompt: {prompt}");
+                Console.Write("> ");
+                string response = Console.ReadLine();
+
+                Entry entry = new Entry();
+                entry._date = DateTime.Now.ToShortDateString();
+                entry._promptText = prompt;
+                entry._entryText = response;
+
+                journal.AddEntry(entry);
+            }
+            else if (choice == 2)
+            {
+                journal.DisplayAll();
+            }
+            else if (choice == 3)
+            {
+                Console.Write("What is the filename? ");
+                string file = Console.ReadLine();
+                journal.SaveToFile(file);
+            }
+            else if (choice == 4)
+            {
+                Console.Write("What is the filename? ");
+                string file = Console.ReadLine();
+                journal.LoadFromFile(file);
+            }
+            else if (choice == 5)
+            {
+                Console.WriteLine("Goodbye!");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
